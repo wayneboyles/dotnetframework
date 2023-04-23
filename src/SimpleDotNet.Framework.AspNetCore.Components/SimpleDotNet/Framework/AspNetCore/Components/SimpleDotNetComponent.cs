e5fbcf231e95c1808b9eebfff6003f4a3a3316ae
Wayne <wayne_boyles@hotmail.com>
2023-04-23 12:06:16 -0500

    Added Component projects

---
+++ src/SimpleDotNet.Framework.AspNetCore.Components/SimpleDotNet/Framework/AspNetCore/Components/SimpleDotNetComponent.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace SimpleDotNet.Framework.AspNetCore.Components
+{
+    public abstract class SimpleDotNetComponent : OwningComponentBase
+    {
+        private ILoggerFactory _loggerFactory;
+
+        private Lazy<ILogger> _lazyLogger => new Lazy<ILogger>(() => LoggerFactory?.CreateLogger(GetType().FullName) ?? NullLogger.Instance, true);
+
+        protected ILoggerFactory LoggerFactory => LazyGetRequiredService(ref _loggerFactory);
+
+        protected ILogger Logger => _lazyLogger.Value;
+
+        [Inject]
+        protected IServiceProvider NonScopedServices { get; set; }
+
+        #region GetService Methods
+
+        protected TService LazyGetRequiredService<TService>(ref TService reference) => LazyGetService(typeof(TService), ref reference);
+
+        protected TService LazyGetNonScopedRequiredService<TService>(ref TService reference) => LazyGetNonScopedRequiredService(typeof(TService), ref reference);
+
+        protected TService LazyGetNonScopedService<TService>(ref TService reference) => LazyGetNonScopedService(typeof(TService), ref reference);
+
+        protected TRef LazyGetService<TRef>(Type serviceType, ref TRef reference)
+        {
+            if (reference == null)
+            {
+                reference = (TRef)ScopedServices.GetService(serviceType)!;
+            }
+
+            return reference;
+        }
+
+        protected TRef LazyGetNonScopedRequiredService<TRef>(Type serviceType, ref TRef reference)
+        {
+            if (reference == null)
+            {
+                reference = (TRef)NonScopedServices.GetRequiredService(serviceType);
+            }
+
+            return reference;
+        }
+
+        protected TRef LazyGetNonScopedService<TRef>(Type serviceType, ref TRef reference)
+        {
+            if (reference == null)
+            {
+                reference = (TRef)NonScopedServices.GetService(serviceType)!;
+            }
+
+            return reference;
+        }
+
+        #endregion
+    }
+}
